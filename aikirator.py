525e220a1919bc94ff181253f2b906805bb78f96
mkachel <maciej.kachel@iphc.cnrs.fr>
2023-03-10 10:23:12 +0100

    Update aikirator.py

--- aikirator.py
+++ aikirator.py
@@ -1,13 +1,17 @@
+#AikiRATOR 
+# TO DO 
+# 1. Add all the special techniques
+# 2. Are all combinations valid? How to filter?
+
 import numpy as np 
 
 import random
-
-
 
 stand =['SUWARI waza', 'TACHI waza','Hanmi handachi waza']
 attack = ['AI HANMI',"GYAKU HANMI","katadori", "katadori-men-uchi","Shomen-uchi","Yokomen-uchi", "ryokata-dori","Ryote-dori", "Morote-dori(katate-ryote)","Muna-dori","Ushiro-ryote-dori","ushiro-kubi-shime","jodan-tsuki","chudan-tsuki"]
 technique = ['ikkyo','nikyo','sankyo','yonkyo','gokyo','shiho-nage','irimi-nage','kote-gaeshi','kaiten-nage','uchi-kaiten-nage', 'koshi-nage','tenchi-nage','juji-garami','kokyu-nage','tenchi-nage',"juji-garami"]
 special=['sumi-otoshi','aiki-otoshi','forgot!']
+
 
 
 stand_i=random.randint(0,len(stand)-1)
